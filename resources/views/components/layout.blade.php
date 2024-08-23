58101bff3775d16c1d4463731de32af3935ae034
Alex Lievense <alexlievense@gmail.com>
2024-08-23 08:08:13 +0200

    feat: add initial tailwind setup

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
-    @vite(['resources/js/app.js'])
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
 <body>
     <div>
@@ -16,8 +16,13 @@
                     <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
                 </a>
             </div>
-            <div>links</div>
-            <div>post a job</div>
+            <div>
+                <a href="">Jobs</a>
+                <a href="">Careers</a>
+                <a href="">Salaries</a>
+                <a href="">Companies</a>
+            </div>
+            <div><a href="">Post a Job</a></div>
         </nav>
         <main>{{ $slot }}</main>
     </div>
