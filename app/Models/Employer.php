16bb56966fb81499fe0ff453f3aa7d26eadba58e
Alex Lievense <alexlievense@gmail.com>
2024-08-23 18:14:33 +0200

    feat: add Job files

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class Employer extends Pivot
 {
-    //
+    use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function jobs(): HasMany
+    {
+        return $this->hasMany(Job::class);
+    }
 }
